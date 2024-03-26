1e9471ccb07a8284baedc56752a94ff0405f5e3c
Cryp3xx <145440436+Cryp3xx@users.noreply.github.com>
2024-03-26 11:03:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+    
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = timeString;
+  }
+  setInterval(updateTime, 1000);
+  updateTime();
